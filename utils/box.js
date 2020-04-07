f81daef3a419714cac0cd26aa7950ac6f41936e8
msaaddev <mrsaadirfan@gmail.com>
2020-04-07 15:55:38 +0500

    👌IMPROVE: box

--- utils/box.js
+++ utils/box.js
@@ -1,20 +1,26 @@
-const {green, red} = require("chalk");
+const { green, red, yellow } = require("chalk");
 
 module.exports = (name, data) => {
   if (name === "✉️  Email") {
-    console.log(green("	-------------------------------------------"));
+    console.log(green("\n	-------------------------------------------"));
     console.log(green("	|                                         |"));
     console.log(green(`	      ${name}: `) + data);
     console.log(green("	|                                         |"));
     console.log(green("	-------------------------------------------"));
   } else if (data === "No Email Found!!") {
-    console.log(red("	-------------------------------------------"));
+    console.log(red("\n	-------------------------------------------"));
     console.log(red("	|                                         |"));
     console.log(red(`	|        ${name}: `) + data + red("     |"));
     console.log(red("	|                                         |"));
     console.log(red("	-------------------------------------------"));
+  } else if (name === "⭐️  Stars") {
+    console.log(yellow("\n	-------------------------------------------"));
+    console.log(yellow("	|                                         |"));
+    console.log(yellow(`	               ${name}: `) + data);
+    console.log(yellow("	|                                         |"));
+    console.log(yellow("	-------------------------------------------"));
   } else {
-    console.log(red("	-------------------------------------------"));
+    console.log(red("\n	-------------------------------------------"));
     console.log(red("	|                                         |"));
     console.log(red(`	|    ${name}: `) + data + red("    |"));
     console.log(red("	|                                         |"));
