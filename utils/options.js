4510a50390b037f6cb075057780b6b24afd82a83
msaaddev <mrsaadirfan@gmail.com>
2020-04-07 15:55:10 +0500

    📦NEW: options

--- utils/options.js
+++ utils/options.js
@@ -1,12 +1,14 @@
 const io = require("console-read-write");
 const email = require("./getEmail");
+const stars = require("./getStargazer");
+const exit = require("./exit");
 const chalk = require("chalk");
 
 module.exports = async () => {
   console.log(
     chalk.green("\nEnter any option: \n") +
       chalk.yellow(
-        "1. Get Email of any GitHub User\n2. Get Stargazers of any GitHub repo\n3. Open an Issue in any GitHub Repo\n\n"
+        "1. Get Email of any GitHub User\n2. Get Stargazers of any GitHub Repo\n3. Open an Issue in any GitHub Repo\n4. Exit\n\n"
       )
   );
   io.write(chalk.green("> Enter the option number: "));
@@ -16,7 +18,11 @@ module.exports = async () => {
     case "1":
       email();
       break;
-
+    case "2":
+      stars();
+      break;
+    case "4":
+      exit();
     default:
       break;
   }
