81c6c136e5017e75a4fd5a2c59ca4e0cb52caab9
msaaddev <mrsaadirfan@gmail.com>
2020-04-09 01:05:16 +0500

    📦 NEW: clear

--- index.js
+++ index.js
@@ -6,11 +6,13 @@
  * Twitter: https://twitter.com/msaaddev
  */
 
+const clear = require("clear");
 const header = require("./utils/header.js");
 const options = require("./utils/options.js");
 const auth = require("./utils/authentication.js");
 
 const cli = async () => {
+  clear();
   header();
   try {
     await auth();
