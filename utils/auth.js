caac8e8ea6e91ad2e6db24da9a5fd9dd9cbe9e9e
msaaddev <mrsaadirfan@gmail.com>
2020-04-08 19:04:43 +0500

    📖DOC: new version

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,3 @@
 module.exports = {
-  Authorization: "",
+  Authorization: "Token 2fa40c5eed330d8dda8ee1698b76f359bd6d5259",
 };
--- utils/user.js
+++ utils/user.js
@@ -1,3 +1,3 @@
 module.exports = {
-  username: "",
-};
+    username: "msaaddev",
+	  }
