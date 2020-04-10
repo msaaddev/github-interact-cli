8ee39fc73170c78fc1cac2007a463e50c539dbfd
msaaddev <mrsaadirfan@gmail.com>
2020-04-10 18:16:34 +0500

    📦 NEW: shortcuts

---
+++ utils/delete.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+/**
+ *
+ * Author: Saad Irfan
+ * GitHub: msaaddev
+ * Twitter: https://twitter.com/msaaddev
+ */
+
+const clear = require("clear");
+const header = require("./header.js");
+const auth = require("./authentication.js");
+const deleteRepo = require("./deleteRepo.js");
+
+const cli = async () => {
+  clear();
+  header();
+  try {
+    await auth();
+  } catch (err) {}
+  deleteRepo();
+};
+
+cli();
